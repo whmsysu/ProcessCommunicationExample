34cc427fc228636098f932095f40eeb033163cba
Haomin Wu <wuhaominsysu@gmail.com>
2018-10-23 03:02:03 +0800

    Add test case

--- app/src/androidTest/java/com/application/haominwu/processcommunicationexample/CommunicationTest.java
+++ app/src/androidTest/java/com/application/haominwu/processcommunicationexample/CommunicationTest.java
@@ -1,14 +1,30 @@
 package com.application.haominwu.processcommunicationexample;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.application.haominwu.processcommunicationexample.activity.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
 public class CommunicationTest {
+
+    @Rule
+    public ActivityTestRule activityTestRule = new ActivityTestRule(MainActivity.class);
+
+
     @Test
     public void communicationTest() {
-
+        onView(withId(R.id.btn_send_message)).perform(click());
+        onView(withId(R.id.tv_message)).check(matches(withText("Test Message")));
     }
 }
